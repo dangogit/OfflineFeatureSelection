883aed3ca66de8e8b2fa42ee423edadecd8f30fc
samuelbenichou <benichoU11>
2022-04-30 17:23:20 +0300

    Update

--- main.py
+++ main.py
@@ -1,79 +1,74 @@
-from skfeature.function.information_theoretical_based import FCBF
-from skfeature.function.information_theoretical_based import MRMR
-from skfeature.function.information_theoretical_based import MIM
-from skfeature.function.similarity_based import lap_score
-from skfeature.utility import construct_W
-from skfeature.utility import unsupervised_evaluation
+# from skfeature.function.information_theoretical_based import FCBF
+# from skfeature.function.information_theoretical_based import MRMR
+# from skfeature.function.information_theoretical_based import MIM
+# from skfeature.function.similarity_based import lap_score
+# from skfeature.utility import construct_W
+# from skfeature.utility import unsupervised_evaluation
 import pandas as pd
 import time
+import glob
+import os
 
+from sklearn.model_selection import cross_val_score
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import MinMaxScaler
+from sklearn import datasets
+from sklearn.svm import LinearSVC
+
+classifiers = {"LinearSVM": LinearSVC(random_state=0, tol=1e-5,max_iter=10000),
+                "KNN": KNeighborsClassifier(n_neighbors=3),
+                "NB": GaussianNB(),
+                "ANN": MLPClassifier(random_state=1, max_iter=300)}
 
 def read_csv_data_from_folder(folder_path):
-    pass
+    dataframes_list = []
+    csv_files = list(filter(lambda f: f.endswith('.csv'), os.listdir(folder_path)))
+    for csv_file in csv_files:
+        temp_df = pd.read_csv(csv_file)
+        dataframes_list.append(temp_df)
+    return dataframes_list
 
 
 def normalize_data(data):
-    pass
+    scaler = MinMaxScaler()
+    scaler.fit(data)
+    return scaler.transform(data)
 
 
-def run_classifier(X, Y, classifier):
-    classifiers = {"LinearSVM": "",
-                   "KNN": "",
-                   "NB": "",
-                   "ANN": ""}
+def run_classifier(classifier, X, y):
+    clf = classifiers[classifier]
+    clf_cv = cross_val_score(clf, X, y, cv=10)
+    print("=== Mean accuracy  ===")
+    print("Mean accuracy Score - {}: ".format(classifier), clf_cv.mean())
+    return clf_cv.mean()
 
-    # return acc
 
-
-def run_fs_method(data, fs, k, classifier):
-    fs_methods = {"ls": lap_score.lap_score,
-                  "fcbf": FCBF.fcbf,
-                  "mrmr": MRMR.mrmr,
-                  "mim": MIM.mim,
+def run_fs_method(fs, k, classifier):
+    fs_methods = {"ls": "",
+                  "fcbf": "",
+                  "mrmr": "",
+                  "mim": "",
                   "skewness": "",
                   "variance": ""}
     # for i in range(k)
     # calculate time and acc for fs method
-    total_acc = 0
-    total_time = 0
-    y = data.iloc[:, -1:]
-    data_values = data.values.astype(float)
-    kwargs_W = {"metric": "euclidean", "neighbor_mode": "knn", "weight_mode": "heat_kernel", "k": k, 't': 1}
-    W = construct_W.construct_W(data, **kwargs_W)
-
-    for i in range(1, k + 1):
-        start = time.time()
-        kwargs = {"X": data_values, "y": y, "W": W, "n_selected_features": i}
-        score = fs_methods[fs](**kwargs)
-        ranking = list(score)[:i]
-        end = time.time()
-        data_cut = data.iloc[:, ranking]
-        data_cut.columns = [''] * len(data_cut.columns)
-        accuracy = run_classifier(data_cut, y, classifier)
-        total_acc += accuracy
-        total_time += end - start
-
-    return total_acc / k, total_time / k
+    # return avc_acc, avg_time
 
 
 def run_simulations(k, classifier, folder_path):
-    fs_methods = ["ls", "fcbf", "mrmr", "mim", "skewness", "variance"]
-    acc_results = pd.DataFrame(columns="Dataset" + fs_methods)
-    time_results = pd.DataFrame(columns="Dataset" + fs_methods)
+    df = read_csv_data_from_folder(folder_path)
+    normalize_data(df)
+    fs_mthods = []
 
-    for data_name, data in read_csv_data_from_folder(folder_path).items():
-        normalize_data(data)
-        data_acc_res = {"Dataset": data_name}
-        data_time_res = {"Dataset": data_name}
-
-        for fs in fs_methods:
-            avc_acc, avg_time = run_fs_method(data, fs, k, classifier)
-            data_acc_res[fs] = avc_acc
-            data_time_res[fs] = avg_time
-
-        acc_results.append(data_acc_res)
-        time_results.append(data_time_res)
+    results = pd.DataFrame()
+    for fs in fs_mthods:
+        avc_acc, avg_time = run_fs_method(fs, k, classifier)
 
 
 def main():
     run_simulations(5, "LinearSVM", "")
+
+if __name__ == "__main__":
+    pass
